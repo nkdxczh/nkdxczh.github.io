24f9e0dee3af28c95e3722ff182a51d0f4cfc20f
nkdxczh <zhc416@lehigh.edu>
2017-07-22 18:48:01 -0400

    Add resume

--- js/click.js
+++ js/click.js
@@ -1,6 +1,10 @@
 function jump(page){
     changePage(page);
-    if(page == 2){
-
+    if(page == 1){
+        $('#resume').show();
+        $('#resume').attr("src",'doc/resume.pdf');
+    }
+    else{
+        $('#resume').hide();
     }
 }
