474a089c80d1c78d7f24d136639f15b7bf41968c
nkdxczh <zhc416@lehigh.edu>
2017-07-28 13:56:22 -0400

    Fix date bug

--- js/clock.js
+++ js/clock.js
@@ -1,4 +1,8 @@
-var endDate = new Date("2094-3-7");
+var dateStr="2094-03-07 00:00:00";
+var a=dateStr.split(" ");
+var d=a[0].split("-");
+var t=a[1].split(":");
+var endDate = new Date(d[0],(d[1]-1),d[2],t[0],t[1],t[2]);
 
 function refreshTime(){
     var last = endDate - Date.now();
